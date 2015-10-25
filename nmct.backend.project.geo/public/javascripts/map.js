c7cb2839c1e1783b0c138802cc7ce28208c54758
ArneTesch <Arne.tesch@student.howest.be>
2015-10-25 12:01:11 +0100

    Deel 1: Frontend GEO APP
    Deel 1 front-end: project GEOFEELINGS

---
+++ nmct.backend.project.geo/public/javascripts/map.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Arne on 10/24/15.
+ */
+var map;
+(function initialize() {
+    var mapCanvas = document.getElementById('map-canvas');
+    var mapOptions = {
+        center: new google.maps.LatLng(50.820301, 3.261038),
+        zoom: 12,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(mapCanvas, mapOptions);
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+    google.maps.event.addDomListener(window, "resize", function() {
+        var center = map.getCenter();
+        google.maps.event.trigger(map, "resize");
+        map.setCenter(center);
+    });
+})();
+
+
+
---
+++ nmct.backend.project.geo/public/javascripts/menu_toggle.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Arne on 10/24/15.
+ */
+<!-- Menu Toggle Script -->
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
