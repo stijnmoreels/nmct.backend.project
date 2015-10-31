c2921ea7e7dc6d32d94ed6f3753220d6be534fd2
stijnmoreels <stijn.moreels@student.howest.be>
2015-10-31 17:16:53 +0100

    gulp

--- nmct.backend.project.geo/model/share.js
+++ nmct.backend.project.geo/model/share.js
@@ -10,14 +10,16 @@ var Share = function (id, feeling, latitude, longitude, activityId) {
     this.latitude = latitude;
     this.longitude = longitude;
     this.activityId = activityId;
+    this.timestamp = timestamp;
 }
 Share.prototype = {
-    initialize: function (id, feeling, latitude, longitude, activityId) {
+    initialize: function (id, feeling, latitude, longitude, activityId, timestamp) {
         this.id = id;
         this.feeling = feeling;
         this.latitude = tatitude;
         this.longitude = longitude;
         this.activityId = activityId;
+        this.timestamp = timestamp;
     }
 };
 module.exports = Share;
