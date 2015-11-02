6788d6aa7e1d3f11d2fb9e9afc5351dd8e93e71f
ArneTesch <Arne.tesch@student.howest.be>
2015-11-02 16:53:08 +0100

    aa

---
+++ nmct.backend.project.geo/public/src/js/dynamicpage.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Arne on 11/2/15.
+ */
+(function dynamicpage(){
+    //check if browser supports history
+    if(Modernizr.history){
+        //alert('ok');
+        var newHash = "",
+            mainContent = document.querySelector(".main-content"),
+            pagewrapper = document.querySelector("#sidebar-wrapper"),
+            baseheight = 0;
+    }
+})();
