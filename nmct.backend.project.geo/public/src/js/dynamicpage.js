aa6a69b16b4c1bac2406a433ca04416ca96b80bb
ArneTesch <Arne.tesch@student.howest.be>
2015-11-04 19:13:51 +0100

    Dynamicpage
    change content without reload

--- nmct.backend.project.geo/public/src/js/dynamicpage.js
+++ nmct.backend.project.geo/public/src/js/dynamicpage.js
@@ -1,67 +1,76 @@
 /*
  * Created by Arne on 11/2/15.*/
+(function () {
 
-function ChangeUrl(title, url) {
-    if (typeof (history.pushState) != "undefined") {
-        var obj = {Title: title, Url: url};
-        history.pushState(obj, obj.Title, obj.Url);
-    } else {
-        alert("Browser does not support HTML5.");
-    }
-}
-
-
-/*
-
-
-(function dynamicpage() {
-    //check if browser supports history
-    if (Modernizr.history) {
-        //alert('ok');
-        var newHash = "",
-            mainContent = document.getElementById("main-content"),
-            pagewrapper = document.getElementById("sidebar-wrapper"),
-            btnHome = document.getElementById("home"),
-            btnLogin = document.getElementById("login"),
-            baseheight = 0,
-            el;
-        baseheight = pagewrapper.offsetHeight - mainContent.offsetHeight;
-        //console.log(baseheight);
-
-        btnHome.addEventListener('click', function () {
-            _link = this.getAttribute("href");
-            history.pushState(null, null, _link);
-            loadContent(_link);
-            return false;
-        });
-
-        btnLogin.addEventListener('click', function () {
-            _link = this.getAttribute("href");
-            history.pushState(null, null, _link);
-            loadContent(_link);
-            return false;
-        });
-
-        function loadContent(href) {
-            mainContent
-                .find("#content")
-                .fadeOut(200, function () {
-                    mainContent.hide().load(href + "#content", function () {
-                        mainContent.fadeIn(200, function () {
-                            pagewrapper.animate({
-                                height: baseheight + mainContent.height() + "px"
-                            });
-                        });
-                        window.onpopstate = function () {
-                            _link = location.pathname.replace(/^.*[\\\/]/, '');
-                            loadContent(_link);
-                        }
-                    });
-                })
+    var pages = {
+        index: {
+            title: "Home Page",
+            url: "pages/index.html",
+            content: ""
+        },
+        login: {
+            title: "Login",
+            url: "pages/login.html",
+            content: ""
+        },
+        register: {
+            title: "Register",
+            url: "pages/register.html",
+            content: ""
         }
+    };
+
+    var navLinks = document.querySelectorAll('.load-content');
+    var contentElements = document.getElementById('content');
+
+    var updateContent = function(stateObj){
+        if(stateObj){
+            contentElements.innerHTML = stateObj.content;
+        }
+    };
+
+    //load the page via AJAX
+    var loadContent = function(url, callback){
+        var request = new XMLHttpRequest();
+
+        request.onload = function(response){
+            //save html in pages object -> don't need to load again
+            pages[url.split('.')[0]].content = response.target.response;
+
+            var pageData = pages[url.split('.')[0]];
+
+            //update content
+            updateContent(pageData);
+            //execute callback
+            callback();
+        };
+        request.open('get','pages/'+url, true);
+        request.send();
+    };
+    // Attach click listeners for each of the nav links.
+    for(var i= 0, l=navLinks.length; i<l; i++){
+        navLinks[i].addEventListener('click', function (e) {
+            e.preventDefault();
+
+            //fetch page data using the url in the link
+            var pageURL = this.attributes['href'].value;
+
+            loadContent(pageURL, function () {
+                var pageData = pages[pageURL.split('.')[0]];
+
+                //create new history item
+                history.pushState(pageData, pageData.title, pageURL);
+            });
+        });
     }
+
+    window.addEventListener('popstate', function(event){
+        updateContent(event.state);
+    });
+
+
+    //load initial content
+    loadContent('index.html', function () {
+        history.replaceState(pages.index, pages.index.title, '');
+    });
 })();
-*/
-
-
-
