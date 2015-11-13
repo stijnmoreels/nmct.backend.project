d3bbda31b999fb66bfd499e2b52340fe11d40f18
ArneTesch <Arne.tesch@student.howest.be>
2015-11-13 16:00:50 +0100

    SpecRunner

--- nmct.backend.project.geo/public/src/dist/js/map.js
+++ nmct.backend.project.geo/public/src/dist/js/map.js
@@ -1,27 +1,27 @@
 /**
  * Created by Arne on 10/24/15.
  */
-var map;
-(function initialize() {
-    var mapCanvas = document.getElementById('map-canvas');
-    var mapOptions = {
-        center: new google.maps.LatLng(50.820301, 3.261038),
-        zoom: 12,
-        mapTypeControl: false,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    map = new google.maps.Map(mapCanvas, mapOptions);
+var map = (function () {
+    "use strict";
+    var initialize = function (element) {};
+    return {
+        initialize: function (element) {
+            var mapCanvas = document.getElementById(element);
+            var mapOptions = {
+                center: new google.maps.LatLng(50.820301, 3.261038),
+                zoom: 12,
+                mapTypeControl: false,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            map = new google.maps.Map(mapCanvas, mapOptions);
 
-    google.maps.event.addDomListener(window, 'load', initialize);
-    google.maps.event.addDomListener(window, "resize", function() {
-        var center = map.getCenter();
-        google.maps.event.trigger(map, "resize");
-        map.setCenter(center);
-    });
-
-    map.addListener('click', function (e) {
-        if(true){
-            placeMarkerAndPanTo(e.latLng, map)
+            google.maps.event.addDomListener(window, 'load', initialize);
+            google.maps.event.addDomListener(window, "resize", function () {
+                var center = map.getCenter();
+                google.maps.event.trigger(map, "resize");
+                map.setCenter(center);
+            });
         }
-    });
+    }
 })();
+
---
+++ nmct.backend.project.geo/public/src/dist/js/mapType.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Arne on 10/29/15.
+ */
+
+var mapType = (function () {
+    "use strict";
+    return {
+        changeMapType: function (type) {
+            switch(type){
+                case 'hybrid':
+                    map.setMapTypeId(google.maps.MapTypeId.HYBRID);
+                    break;
+                case 'roadmap':
+                    map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
+                    break;
+                case 'satellite':
+                    map.setMapTypeId(google.maps.MapTypeId.SATELLITE);
+                    break;
+                case 'terrain':
+                    map.setMapTypeId(google.maps.MapTypeId.TERRAIN);
+                    break;
+                default:
+                    map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
+            }
+        }
+    }
+})();
