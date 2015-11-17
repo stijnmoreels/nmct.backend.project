0fc7f399ce17f8a7c6b8005fe9ee7edc5cdc6d97
ArneTesch <Arne.tesch@student.howest.be>
2015-11-17 10:37:06 +0100

    aa

--- nmct.backend.project.geo/public/src/Controllers/LoginController.js
+++ nmct.backend.project.geo/public/src/Controllers/LoginController.js
@@ -3,32 +3,20 @@
 
     var LoginController = function ($scope, $location) {
 
-<<<<<<< HEAD
-
-        $scope.userLogin = function () {
-            client.login($scope.username, $scope.password, function (error, user) {
-=======
-        //$scope.login = login;
 
         $scope.userLogin = function (username, password) {
             client.login(username, password, function (error, user) {
->>>>>>> origin/master
                 if (error) {
                     console.log(error);
                 } else {
                     $location.path("/main");
                 }
                });
-<<<<<<< HEAD
             console.log("username:  " + $scope.username + ", password: " + $scope.password);
-=======
             //alert('ok');
->>>>>>> origin/master
         };
 
     };
-
-
 
     app.controller("LoginController", LoginController);
 })();
