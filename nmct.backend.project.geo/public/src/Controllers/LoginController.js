8eda870c26de80893eacd889540e4e5271b760f6
ArneTesch <Arne.tesch@student.howest.be>
2015-11-20 15:04:43 +0100

    Access restriction

--- nmct.backend.project.geo/public/src/Controllers/LoginController.js
+++ nmct.backend.project.geo/public/src/Controllers/LoginController.js
@@ -1,20 +1,21 @@
 (function () {
     var app = angular.module("app");
 
-    var LoginController = function ($scope, $location) {
+    var LoginController = function ($scope, $location, $rootScope) {
 
         $scope.userLogin = function () {
 
-            $location.path('/main');
+            //$location.path('/main');
 
             client.login($scope.username, $scope.password, function (error, user) {
                 if (error) {
                     console.log(error);
                 } else {
+                    $rootScope.loggedInUser = $scope.username;
                     $location.path("/main");
                 }
             });
         };
     };
-    app.controller("LoginController", ["$scope", "$location", LoginController]);
+    app.controller("LoginController", ["$scope", "$location","$rootScope", LoginController]);
 })();
--- nmct.backend.project.geo/public/src/js/app.js
+++ nmct.backend.project.geo/public/src/js/app.js
@@ -11,43 +11,52 @@
             .when('/', {
                 controller: 'LoginController',
                 controllerAs: 'login',
-                templateUrl:'./templates/login.html'
+                templateUrl: './templates/login.html'
             })
             .when('/register', {
                 controller: 'RegisterController',
                 controllerAs: 'register',
                 templateUrl: './templates/register.html'
             })
-            .when('/main',{
+            .when('/main', {
                 controller: 'MainController',
                 controllerAs: 'main',
                 templateUrl: './templates/main.html'
             })
             .otherwise({
-                redirectTo:'/'
+                redirectTo: '/'
             });
-    });
+    }).run(function ($rootScope, $location) {
+            $rootScope.$on("$routeChangeStart", function (event, next, current) {
+                if ($rootScope.loggedInUser == null) {
+                    //console.log("no user");
+                    if (next.templateUrl === "./templates/main.html") {
+                        $location.path("/");
+                    }
+                }
+            })
+        });
 
 })();
 
 /*
-var roadmap = document.getElementById("roadmap");
-var hybrid = document.getElementById("hybrid");
-var satellite = document.getElementById("satellite");
-var terrain = document.getElementById("terrain");
+ var roadmap = document.getElementById("roadmap");
+ var hybrid = document.getElementById("hybrid");
+ var satellite = document.getElementById("satellite");
+ var terrain = document.getElementById("terrain");
 
-roadmap.addEventListener('click', function () {
-    mapType.changeMapType("roadmap");
-});
+ roadmap.addEventListener('click', function () {
+ mapType.changeMapType("roadmap");
+ });
 
-hybrid.addEventListener('click', function () {
-    mapType.changeMapType("hybrid");
-});
+ hybrid.addEventListener('click', function () {
+ mapType.changeMapType("hybrid");
+ });
 
-satellite.addEventListener('click', function () {
-    mapType.changeMapType("satellite");
-});
+ satellite.addEventListener('click', function () {
+ mapType.changeMapType("satellite");
+ });
 
-terrain.addEventListener('click', function () {
-    mapType.changeMapType("terrain");
-});*/
+ terrain.addEventListener('click', function () {
+ mapType.changeMapType("terrain");
+ });*/
