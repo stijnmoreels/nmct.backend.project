8048c0f7c738eb9e17ec9b32976669178cdd9a13
ArneTesch <Arne.tesch@student.howest.be>
2015-11-22 10:39:17 +0100

    aa

--- nmct.backend.project.geo/public/src/Controllers/MainController.js
+++ nmct.backend.project.geo/public/src/Controllers/MainController.js
@@ -3,7 +3,19 @@
 
     var MainController = function ($scope) {
 
+        var allShares = [];
+        $scope.getShares = (function () {
+            client.getShares(function (error, shares) {
+                if(error){
+                    console.log(error);
+                }else {
+                    allShares = shares;
+                }
+            });
+        })();
+
+
     };
 
-    app.controller("MainController", MainController);
+    app.controller("MainController", ["$scope",MainController]);
 })();
