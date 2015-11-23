d21ff184265c81635d62457c1e788a78c49d2558
stijnmoreels <stijn.moreels@student.howest.be>
2015-11-23 16:24:46 +0100

    isActivity fix

--- nmct.backend.project.geo/public/src/Controllers/MainController.js
+++ nmct.backend.project.geo/public/src/Controllers/MainController.js
@@ -1,39 +1,38 @@
 (function () {
     var app = angular.module("app");
-
+    
     var MainController = function ($scope, $rootScope) {
-
-        var isActivity = document.getElementById("isActivity").value;
-
+        
+        var isActivity = document.getElementById("isActivity").checked;
+        
         $scope.addShareDb = function () {
-
-            if(isActivity){
+            
+            if (isActivity) {
                 var lat, lng;
                 var location = navigator.geolocation.getCurrentPosition(getPosition, showError);
-                function getPosition(position){
+                function getPosition(position) {
                     lat = position.coords.latitude;
                     lng = position.coords.longitude;
-
-                    activityModel.id = new Date().getDate() + "-"+ $rootScope.loggedInUser;
+                    
+                    activityModel.id = new Date().getDate() + "-" + $rootScope.loggedInUser;
                     activityModel.feeling = $scope.feelings;
                     activityModel.latitude = lat;
                     activityModel.longitude = lng;
                     activityModel.timestamp = new Date().getDate();
                     activityModel.author = $rootScope.loggedInUser;
-
+                    
                     client.addActivity(activityModel, function (error, activity) {
-                       // addActivityToMap(error, activity);
+                        // addActivityToMap(error, activity);
                         console.log("added");
                     });
                 }
-
-
-            }else{
+            } else {
+                
             }
         };
 
 
     };
-
-    app.controller("MainController", ["$scope","$rootScope",MainController]);
+    
+    app.controller("MainController", ["$scope", "$rootScope", MainController]);
 })();
