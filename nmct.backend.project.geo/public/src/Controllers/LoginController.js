b51bc1783890eb695d1b310ad423f875b92ddcbc
ArneTesch <Arne.tesch@student.howest.be>
2015-12-01 12:28:58 +0100

    login update

--- nmct.backend.project.geo/public/src/Controllers/LoginController.js
+++ nmct.backend.project.geo/public/src/Controllers/LoginController.js
@@ -7,6 +7,26 @@
         var allActivities = [];
         
         $scope.userLogin = function () {
+            //$location.path("/main");
+
+            $rootScope.loggedInUser = null;
+            client.login($scope.username, $scope.password, function (error, user) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    //$cookieStore.put("username", $scope.username);
+                    $rootScope.loggedInUser = $scope.username;
+                    localStorage.username = $scope.username;
+                    /*$location.path("/main");*/
+                    location.href = "/#/main";
+                }
+            });
+        };
+
+        //document.querySelector("#loginButton").addEventListener("click", l);
+        var l = function () {
+            //$location.path("/main");
+
             $rootScope.loggedInUser = null;
             client.login($scope.username, $scope.password, function (error, user) {
                 if (error) {
