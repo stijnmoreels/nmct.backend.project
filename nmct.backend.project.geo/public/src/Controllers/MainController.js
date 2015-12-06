2e1cbc609f700efb8bc79071b063b6a30aee8b73
ArneTesch <Arne.tesch@student.howest.be>
2015-12-06 19:07:44 +0100

    update main
    - hidden radiobuttons

--- nmct.backend.project.geo/public/src/Controllers/MainController.js
+++ nmct.backend.project.geo/public/src/Controllers/MainController.js
@@ -1,65 +1,62 @@
 (function () {
     var app = angular.module("app");
-    
+
     var MainController = function ($scope, $rootScope, $location) {
-        
-
-            $scope.addActivityDb = function () {
-
-                var lat, lng;
-                var location = navigator.geolocation.getCurrentPosition(getPosition, showError);
-                var target = event.target || event.srcElement || event.originalTarget;
-                var feeling = target.value;
-                var btn = document.getElementById(feeling);
-
-                function getPosition(position) {
-                    lat = position.coords.latitude;
-                    lng = position.coords.longitude;
 
 
+        $scope.addActivityDb = function () {
 
-                    if($scope.activityName){
-                        activityModel.id = new Date().getTime() + "-" + $rootScope.loggedInUser;
-                        activityModel.activityName = $scope.activityName;
-                        activityModel.feeling = $scope.feeling;
-                        activityModel.latitude = lat;
-                        activityModel.longitude = lng;
-                        activityModel.timestamp = new Date().toLocaleDateString();
-                        activityModel.author = $rootScope.loggedInUser;
+            var lat, lng;
+            var location = navigator.geolocation.getCurrentPosition(getPosition, showError);
 
-                        client.addActivity(activityModel, function (error, activity) {
-                            //addActivityToMap(error, activity);
-                            console.log("activity added");
-                        });
-                    }else{
-                        shareModel.activityId = 0;
-                        shareModel.id = new Date().getTime() + "-"+ $rootScope.loggedInUser;
-                        shareModel.feeling = $scope.feeling;
-                        shareModel.latitude = lat;
-                        shareModel.longitude = lng;
-                        shareModel.timestamp = new Date().toLocaleDateString();
-                        shareModel.author = $rootScope.loggedInUser;
 
-                        client.addShare(shareModel, function (error, share) {
-                           console.log("share added")
-                        });
+            function getPosition(position) {
+                lat = position.coords.latitude;
+                lng = position.coords.longitude;
 
-                    }
+
+                if ($scope.activityName) {
+                    activityModel.id = new Date().getTime() + "-" + $rootScope.loggedInUser;
+                    activityModel.activityName = $scope.activityName;
+                    activityModel.feeling = $scope.feeling;
+                    activityModel.latitude = lat;
+                    activityModel.longitude = lng;
+                    activityModel.timestamp = new Date().toLocaleDateString();
+                    activityModel.author = $rootScope.loggedInUser;
+
+                    client.addActivity(activityModel, function (error, activity) {
+                        //addActivityToMap(error, activity);
+                        console.log("activity added");
+                    });
+                } else {
+                    shareModel.activityId = 0;
+                    shareModel.id = new Date().getTime() + "-" + $rootScope.loggedInUser;
+                    shareModel.feeling = $scope.feeling;
+                    shareModel.latitude = lat;
+                    shareModel.longitude = lng;
+                    shareModel.timestamp = new Date().toLocaleDateString();
+                    shareModel.author = $rootScope.loggedInUser;
+
+                    client.addShare(shareModel, function (error, share) {
+                        console.log("share added")
+                    });
 
                 }
-            };
 
-        
+            }
+        };
+
+
         $scope.logout = function () {
             $rootScope.loggedInUser = null;
             $location.path("/");
         };
-        
+
         $scope.changeMap = function () {
             var target = event.target || event.srcElement || event.originalTarget;
             mapType.changeMapType(target.id);
         };
     };
-    
+
     app.controller("MainController", ["$scope", "$rootScope", "$location", MainController]);
 })();
