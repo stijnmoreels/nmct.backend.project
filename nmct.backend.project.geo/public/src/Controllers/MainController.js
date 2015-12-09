6b848dbbc77f6f8e2535ba05ea39a5c0e8e3c8cd
stijnmoreels <stijn.moreels@student.howest.be>
2015-12-09 09:49:27 +0100

    Setup click handler in MainController (sendMessage)

--- nmct.backend.project.geo/public/src/Controllers/MainController.js
+++ nmct.backend.project.geo/public/src/Controllers/MainController.js
@@ -1,39 +1,39 @@
 (function () {
     var app = angular.module("app");
-
+    
     var MainController = function ($scope, $rootScope, $location) {
         
         // pure designwise, has no security issues
         //$scope.isAdmin = localStorage.isAmin == undefined || !localStorage.isAdmin ? false : true;
         $scope.isAdmin = false;
-
+        
         var feelings = ["happy", "excited", "tender", "sad", "scared", "angry"];
         for (var i = 0, l = feelings.length; i < l; i++) {
             (function (i) {
                 // Click listener for button within infowindow
-                $("#"+feelings[i]).bind("click", function (e) {
-                    if(feelings[i] == $scope.feeling){
-                        $("#"+$scope.feeling).addClass("active");
-                    }else {
-                        $("#"+$scope.feeling).removeClass("active");
+                $("#" + feelings[i]).bind("click", function (e) {
+                    if (feelings[i] == $scope.feeling) {
+                        $("#" + $scope.feeling).addClass("active");
+                    } else {
+                        $("#" + $scope.feeling).removeClass("active");
                     }
                 });
             })(i);
         }
-
+        
         $scope.addActivityDb = function () {
-
+            
             var lat, lng;
             var location = navigator.geolocation.getCurrentPosition(getPosition, showError);
-
+            
             /*var feeling = $('.feelings input:radio:checked').val()*/
-
-
+            
+            
             function getPosition(position) {
                 lat = position.coords.latitude;
                 lng = position.coords.longitude;
-
-
+                
+                
                 if ($scope.activityName) {
                     activityModel.id = new Date().getTime() + "-" + $rootScope.loggedInUser;
                     activityModel.activityName = $scope.activityName;
@@ -42,7 +42,7 @@
                     activityModel.longitude = lng;
                     activityModel.timestamp = new Date().toLocaleDateString();
                     activityModel.author = $rootScope.loggedInUser;
-
+                    
                     client.addActivity(activityModel, function (error, activity) {
                         //addActivityToMap(error, activity);
                         console.log("activity added");
@@ -55,7 +55,7 @@
                     shareModel.longitude = lng;
                     shareModel.timestamp = new Date().toLocaleDateString();
                     shareModel.author = $rootScope.loggedInUser;
-
+                    
                     client.addShare(shareModel, function (error, share) {
                         console.log("share added")
                     });
@@ -65,16 +65,29 @@
             }
         };
         
+        $scope.sendMessage = function () {
+            // TODO: get selected user (li with id == socketId)
+            // TODO: get message typed by user
+            // TODO: send message to this user
+            // TIP: only send when a user is selected
+            
+
+            //client.sendMessage(message, socketId, function (error, message) {
+            //    if(error) { console.log(error); }
+            //    console.log(message);
+            //});
+        };
+
         $scope.logout = function () {
             $rootScope.loggedInUser = null;
             $location.path("/");
         };
-
+        
         $scope.changeMap = function () {
             var target = event.target || event.srcElement || event.originalTarget;
             mapType.changeMapType(target.id);
         };
     };
-
+    
     app.controller("MainController", ["$scope", "$rootScope", "$location", MainController]);
 })();
