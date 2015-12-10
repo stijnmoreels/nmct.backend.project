a2cfd780d17c474ed70735f0c29549073b2cf19a
stijnmoreels <stijn.moreels@student.howest.be>
2015-12-10 16:29:42 +0100

    Regular Expressions Activity Name

--- nmct.backend.project.geo/public/src/Controllers/MainController.js
+++ nmct.backend.project.geo/public/src/Controllers/MainController.js
@@ -23,6 +23,11 @@
         
         $scope.addActivityDb = function () {
             
+            // block event if the input field (name of activity) doesn't match the Regular Expression
+            var regularExpression = new RegExp("^[a-zA-Z]*$");
+            if (!regularExpression.test($scope.activityName))
+                return;
+
             var lat, lng;
             var location = navigator.geolocation.getCurrentPosition(getPosition, showError);
             
