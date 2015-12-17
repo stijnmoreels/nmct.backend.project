8b6347e3235155bb4901ce8c72275799d6ada9f4
ArneTesch <Arne.tesch@student.howest.be>
2015-12-17 09:25:07 +0100

    ChartJS integration: unsigned share

--- nmct.backend.project.geo/public/src/js/addMarker.js
+++ nmct.backend.project.geo/public/src/js/addMarker.js
@@ -274,16 +274,48 @@ function addUnsignedShareToMap(error, share) {
     var userShares = allUnsignedShares[share.author];
     var contentStringShare = '<div id="iw-container" class="container">' +
                                 '<div class="row">' +
-                                    '<h1 class="iw-title">@' + share.author + '</h1>' + 
-                                    '<p>' + share.feeling + '</p>' +
+                                    '<h1 class="iw-title">' + share.author + '</h1>' +
+                                    '<canvas id="feelings-chart"></canvas>' +
                                 '</div>' + 
                              '</div>';
-    
+
+
     var infoWindow = new google.maps.InfoWindow({
         content: contentStringShare
     });
-    
+
+
+    //Example data ! 
+    var data = {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: [65, 59, 80, 81, 56, 55, 40]
+            },
+            {
+                label: "My Second dataset",
+                fillColor: "rgba(151,187,205,0.2)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: [28, 48, 40, 19, 86, 27, 90]
+            }
+        ]
+    };
+
     marker.addListener('click', function () {
         infoWindow.open(map, marker);
+        var ctx = document.getElementById("feelings-chart").getContext("2d");
+        var myLineChart = new Chart(ctx).Line(data);
+
     });
 }
