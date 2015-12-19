5c127b545c7d35afe66a815bb7ea7881d31f1988
ArneTesch <Arne.tesch@student.howest.be>
2015-12-19 15:09:11 +0100

    hide/show password - Register

--- nmct.backend.project.geo/public/src/Controllers/RegisterController.js
+++ nmct.backend.project.geo/public/src/Controllers/RegisterController.js
@@ -5,6 +5,23 @@
 
         $rootScope.loggedInUser = null;
 
+        //toggle show/hide password
+        // Example 2
+        $('#password').hideShowPassword({
+            // Make the password visible right away.
+            show: false,
+            // Create the toggle goodness.
+            innerToggle: true,
+            // Give the toggle a custom class so we can style it
+            // separately from the previous example.
+            toggleClass: 'my-toggle-class',
+            // Don't show the toggle until the input triggers
+            // the 'focus' event.
+            hideToggleUntil: 'focus',
+            // Enable touch support for toggle.
+            touchSupport: Modernizr.touch
+        });
+
 
         $scope.userRegister = function () {
             // block event if the input fields doesn't match the Regular Expression
