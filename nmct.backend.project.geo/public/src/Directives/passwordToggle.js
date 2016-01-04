e554897a63fcfdd76156853b4f24e5fbea5a9a03
ArneTesch <Arne.tesch@student.howest.be>
2016-01-04 11:36:09 +0100

    JS minify

--- nmct.backend.project.geo/public/src/Directives/passwordToggle.js
+++ nmct.backend.project.geo/public/src/Directives/passwordToggle.js
@@ -8,10 +8,10 @@ directives.directive('passwordToggle', function ($compile) {
         link: function (scope, elem, attrs) {
             scope.tgl = function () {
                 elem.attr('type', (elem.attr('type') === 'text' ? 'password' : 'text'));
-            }
+            };
             var lnk = angular.element('<a data-ng-click="tgl()">Toggle</a>');
             $compile(lnk)(scope);
             elem.wrap('<div class="password-toggle"/>').after(lnk);
         }
-    }
+    };
 });
